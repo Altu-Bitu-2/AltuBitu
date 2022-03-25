9743d549ad912f95e1dda5bb0d54699ae09a3d43
Jiwon Yoon <jiwon105020@gmail.com>
2022-03-25 18:24:07 +0900

    [브루트포스] 220325 - Update

--- 03월 18일 - 브루트포스/17626.cpp
+++ 03월 18일 - 브루트포스/17626.cpp
@@ -3,19 +3,17 @@ using namespace std;
 
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    int arr[50005], n;
-    for(int i = 1; i <= 50000; i++){
-        arr[i] = 4;
-    }
-    for(int i = 1; i*i <= 50000; i++){
+    vector<int> arr(50005, 4);
+    int n;
+    cin >> n;
+    for(int i = 1; i*i <= 50000 && i*i <= n; i++){
         arr[i*i] = 1;
-        for(int j = 1; i*i + j*j <= 50000; j++){
+        for(int j = 1; i*i + j*j <= 50000 && i*i + j*j <= n; j++){
             if(arr[i*i+j*j] >2) arr[i*i + j*j] = 2;
-            for(int k = 1;i*i + j*j + k*k <= 50000; k++){
+            for(int k = 1;i*i + j*j + k*k <= 50000 && i*i + j*j+k*k <= n; k++){
                 if(arr[i*i+j*j+k*k] >3)arr[i*i + j*j + k*k] = 3;
             }
         }
     }
-    cin >> n;
     cout << arr[n]<<"\n";
 }
